5214e510f4d64256f15de7350c1bab0b077ccc4a
Rima Bachar <bachar.rima@etu.umontpellier.fr>
2017-02-26 19:15:23 +0100

    Debugging and optimization: restructuring code, changing parameters names in certain callback functions, disabling login button after joining game, re-enabling login button after quitting game for possible re-joining of game, making sure that players have unique aliasnames upon joining game (requires additional attention later with the usage of regex), debugging rejoindre() to preserve data upon client's page refresh, replacing constant numbers with constant variables, renaming player object constructor, added node modules to interact with mongodb

--- client.js
+++ client.js
@@ -1,89 +1,136 @@
-var socket;
-var nbOfPlayers = 0;
-var players = [];
-var localPlayer = -1;
-var nPlayer = null;
+var socket,
+  nbOfPlayers = 0,
+  players = [],
+  localPlayer = -1,
+  MAX_PLAYERS_NB = 5;
 
+//Client connects to the server and sends state data
+socket = io("http://localhost:8888");
+socket.emit("etat", {});
 
-// Function called by clicking button to join the game
-function rejoindrePartie() {
-  if (localPlayer == -1) {
-    playerName = document.getElementsByName('player')[0].value;
-    if (nbOfPlayers < 5) {
-      if (playerName != "") {
-        console.log("Envoi de la connexion");
-        socket.emit("rejoindre", { "playerName": playerName });
-        localPlayer = nbOfPlayers;
+//Client receives state data from server and updates client-side data
+socket.on("etat", function(state_data) {
+  console.log("Dans la réception d'état");
+
+  for (let d in state_data) {
+    console.log(d + " : " + state_data[d]);
+
+    if (d == "players"){
+      var pl = state_data[d];
+      if (pl.length){
+        for (let i=0; i<pl.length; i++){
+          console.log("player = " + pl[i].aliasName);
+          players.push(pl[i].aliasName);
+          document.getElementById("player"+i).innerHTML = pl[i].aliasName;
+          document.getElementById("attack"+i).innerHTML = pl[i].attack;
+          document.getElementById("defense"+i).innerHTML = pl[i].defense;
+        }
       }
     }
-    else {
-      console.log("Vous ne pouvez pas pour l'instant rejoindre le groupe !");
-    }
-  }
-}
-
-// Function called by clicking button to log out
-function quitterPartie() {
-  console.log("Dans quitterPartie");
-  if (localPlayer > -1) {
-    console.log("Suppression du joueur n."+localPlayer);
-    socket.emit("quitter", { "playerNum": localPlayer} );
-  }
-}
-
-// Activates card drawing phase server-side
-function drawCard(){
-  socket.emit('playerTurn', {"playerNum": localPlayer});
-}
-
-socket = io('http://localhost:8888');
-socket.emit("etat",{});
-
-// Receives state from server and updates data
-socket.on("etat", function(data) {
-  console.log("Dans la réception d'état");
-  for (var m in data) {
-    console.log(m+" : "+data[m]);
-    window[m] = data[m];
-    for (var i=0; i < players.length; i++) {
-      console.log("player ="+players[i].aliasName);
-      document.getElementById("player"+i).innerHTML = players[i].aliasName;
-      document.getElementById("attack"+i).innerHTML = players[i].attack;
-      document.getElementById("defense"+i).innerHTML = players[i].defense;
+    else{
+      window[d] = state_data[d];
     }
   }
 });
 
-// Adds a player in the game: adds the player's name in the players array
-// Shows the player's name, attack and defense
-socket.on("newPlayer", function(data) {
+//Joining the game by clicking on a button
+function rejoindrePartie() {
+  if (localPlayer == -1) {
+    playerName = document.getElementsByName("player")[0].value;
+    console.log("players: " + players);
+
+    if (nbOfPlayers < MAX_PLAYERS_NB) {
+      if (playerName != "" && !players.includes(playerName)) {
+        console.log("Envoi de la connexion");
+        localPlayer = nbOfPlayers;
+
+        $("button[id=join]").attr("disabled", "disabled");
+        $("input[name=player]").attr("disabled", "disabled");
+        $("input[name=player]").val("");
+
+        socket.emit("rejoindre", {"playerName": playerName});
+      }
+      else
+        console.log("Vous n'avez fournis aucun pseudo ou votre pseudo est déjà pris par un autre joueur");
+    }
+    else
+      console.log("Vous ne pouvez pas pour l'instant rejoindre le groupe !");
+  }
+}
+
+/*
+  Adding a player to the game:
+  - Adds the player's name to the players array
+  - Shows the player's name, attack and defense in the html page
+*/
+socket.on("newPlayer", function(player_data) {
   console.log("Du serveur : nouveau joueur");
-  players.push(data["playerName"]);
-  document.getElementById("player"+nbOfPlayers).innerHTML = data["playerName"];
-  document.getElementById("attack"+nbOfPlayers).innerHTML = data["playerAttack"];
-  document.getElementById("defense"+nbOfPlayers).innerHTML = data["playerDefense"];
+
+  players.push(player_data.playerName);
+  document.getElementById("player"+nbOfPlayers).innerHTML = player_data.playerName;
+  document.getElementById("attack"+nbOfPlayers).innerHTML = player_data.playerAttack;
+  document.getElementById("defense"+nbOfPlayers).innerHTML = player_data.playerDefense;
+
   nbOfPlayers++;
 });
 
-// Removes a player from the game: removes the name, attack and defense, and
-// the name from the players array
-socket.on("offlinePlayer", function(data) {
-  var playerNum = data['playerNum'];
-  var offlinePlayer = players[playerNum];
-  console.log("Du serveur offlinePlayer = "+offlinePlayer+" (joueur n."+playerNum+")");
-  if (localPlayer == playerNum){
+/*
+  Receiving the player's turn status:
+  - If it's the local player's turn, enables the button to draw a card
+  - Else, disables it
+*/
+socket.on("status", function(status_data){
+  console.log("En réception du statut de tour du joueur")
+
+  if(status_data.playerStatus == 1){
+    console.log("Tour du joueur " + status_data.playerName);
+
+    if(status_data.playerNum == localPlayer)
+      $("button[id=draw]").removeAttr("disabled");
+  }
+  else{
+    if(status_data.playerNum == localPlayer)
+      $("button[id=draw]").attr("disabled", "disabled");
+  }
+});
+
+//Quitting the game by clicking on a button
+function quitterPartie() {
+  console.log("Dans quitterPartie");
+
+  if (localPlayer > -1) {
+    console.log("Suppression du joueur n." + localPlayer);
+
+    $("button[id=join]").removeAttr("disabled");
+    $("input[name=player]").removeAttr("disabled");
+
+    socket.emit("quitter", {"playerNum": localPlayer});
+  }
+}
+
+/*
+Removing a player from the game:
+  - Removes the name from the players array
+  - Removing the name, attack and defense from the html page
+*/
+socket.on("offlinePlayer", function(offlinePlayer_data) {
+  var playerNum = offlinePlayer_data.playerNum,
+    offlinePlayer = players[playerNum];
+
+  console.log("Du serveur offlinePlayer = " + offlinePlayer + " (joueur n." + playerNum + ")");
+
+  if (localPlayer == playerNum)
     localPlayer = -1;
-  }
-  else if (localPlayer > 0){
+  else if (localPlayer > 0)
     localPlayer--;
-  }
-  console.log("localPlayer = "+localPlayer);
+
   players.splice(playerNum, 1);
   nbOfPlayers--;
-  for (var i=playerNum; i < nbOfPlayers; i++){
-    document.getElementById("player"+i).innerHTML = players[i].aliasName;
-    document.getElementById("attack"+i).innerHTML = players[i].attack;
-    document.getElementById("defense"+i).innerHTML = players[i].defense;
+
+  for (var i=playerNum; i <nbOfPlayers; i++){
+    document.getElementById("player"+i).innerHTML = players[i];
+    document.getElementById("attack"+i).innerHTML = offlinePlayer_data.players[i].attack;
+    document.getElementById("defense"+i).innerHTML = offlinePlayer_data.players[i].defense;
   }
   document.getElementById("player"+i).innerHTML = "";
   document.getElementById("attack"+i).innerHTML = "";
@@ -91,24 +138,12 @@ socket.on("offlinePlayer", function(data) {
 });
 
 
-// Shows the card drawn by the active player
-socket.on('cardDrawn', function(data){
-  document.getElementById("card_num").innerHTML = data["cardNum"];
-});
+//Activates the server-side card drawing phase
+function drawCard(){
+  socket.emit("playerTurn", {"playerNum": localPlayer});
+}
 
-// Receives player turn status. If it's the local player's turn, enables the
-// button to draw a card. Else, disables it.
-socket.on('status', function(data){
-  console.log("En réception du statut de tour du joueur")
-  if(data["playerStatus"] == 1){
-    console.log("Tour du joueur "+data["playerName"]);
-    if(data["playerNum"] == localPlayer){
-      $("button[id=draw]").removeAttr('disabled');
-    }
-  }
-  else{
-    if(data["playerNum"] == localPlayer){
-      $("button[id=draw]").attr("disabled", "disabled");
-    }
-  }
+//Shows the active player's drawn card
+socket.on("cardDrawn", function(card_data){
+  document.getElementById("card_num").innerHTML = card_data.cardNum;
 });
--- server.js
+++ server.js
@@ -1,84 +1,107 @@
-var players = [];
-var nbOfPlayers = 0;
-var cards = [{'num':0}, {'num':1}, {'num':2}, {'num':3}, {'num':4}, {'num':5}];
-var currentPlayer = null, nPlayer = null;
-var basicAttack = 5, basicDefense = 5;
+var players = [],
+  nbOfPlayers = 0,
+  BASIC_ATTACK = 5,
+  BASIC_DEFENSE = 5;
+var cards = [{"num":0}, {"num":1}, {"num":2}, {"num":3}, {"num":4}, {"num":5}];
 
-var app = require('http').createServer(function(req, res){});
+//Setting the server and making it listen at port 8888
+var app = require("http").createServer(function(req, res){});
 app.listen(8888);
+
+//Setting the socket between the client and the server
 var io = require("socket.io").listen(app);
 
-// Player constructor
-function createPlayer(alias, att, def, s){
-  this.aliasName = alias;
-  this.attack = att;
-  this.defense = def;
-  this.status = s;
-}
-
-// Draws a random cards and sends cards data to the client
-function cardDraw(){
-  var cardNum = Math.floor(Math.random() * 5);
-  io.emit('cardDrawn', {"cardNum":cardNum});
-}
-
-// Sends data state to the client
-io.sockets.on('connection', function (socket) {
-  socket.on('etat', function(message) {
-    var etat = {"nbOfPlayers":nbOfPlayers, "players":players};
-    io.emit('etat', etat);
+io.sockets.on("connection", function (socket) {
+  //Sending server-side state data to client upon connection
+  socket.on("etat", function() {
+    var state_data = {"nbOfPlayers": nbOfPlayers, "players": players};
+    io.emit("etat", state_data);
   });
 
-  // Creates a player in the array and sends player's data to the client
-  socket.on('rejoindre',function(message) {
-    var turnStatus = 0;
-    playerName = message["playerName"];
-    // If first player, activates the player's turn
-    if(nbOfPlayers == 0){
+  //Player object constructor
+  function Player(alias, att, def, s){
+    this.aliasName = alias;
+    this.attack = att;
+    this.defense = def;
+    this.status = s;
+  }
+
+  /*
+  Creating a new player:
+    - Adding the player to the players array
+    - Sends the player's data and turnstatus_data to the client
+  */
+  socket.on("rejoindre", function(player_data) {
+    var turnStatus = 0,
+      playerName = player_data.playerName;
+
+    //If first player, activates the player's turn
+    if(!nbOfPlayers)
       turnStatus = 1;
-    }
-    nPlayer = new createPlayer(playerName, basicAttack, basicDefense, turnStatus);
+
+    var nPlayer = new Player(playerName, BASIC_ATTACK, BASIC_DEFENSE, turnStatus);
     players.push(nPlayer);
-    io.emit('newPlayer', {"playerNum":nbOfPlayers,
-                          "playerName":playerName,
-                          "playerAttack": nPlayer.attack,
-                          "playerDefense": nPlayer.defense
-                        });
-    io.emit('status', {"playerStatus": turnStatus,
-                       "playerName": playerName,
-                       "playerNum": nbOfPlayers }
-                      );
+    io.emit("newPlayer",
+      {
+        "playerNum": nbOfPlayers,
+        "playerName": playerName,
+        "playerAttack": nPlayer.attack,
+        "playerDefense": nPlayer.defense
+      });
+
+    io.emit("status",
+      {"playerStatus": turnStatus,
+        "playerNum": nbOfPlayers,
+       "playerName": playerName
+      });
+
    nbOfPlayers++;
   });
 
-  // Removes a player from the array
-  socket.on('quitter',function(message) {
-    playerNum = message["playerNum"];
+  //Removing the player from the players array
+  socket.on("quitter", function(player_data) {
+    var playerNum = player_data.playerNum;
     players.splice(playerNum, 1);
     nbOfPlayers--;
-    io.emit('offlinePlayer', message);
+
+    var offlinePlayer_data =
+      {
+        "playerNum": playerNum,
+        "players": players
+      };
+    io.emit("offlinePlayer", offlinePlayer_data);
   });
 
-  // Calls function tp draw card, activates next player's turn
-  socket.on('playerTurn', function(data){
+  //Drawing a random card and sending its data to the client
+  function cardDraw(){
+    var cardNum = Math.floor(Math.random() * 5);
+    io.emit("cardDrawn", {"cardNum":cardNum});
+  }
+
+  //Calls function to draw card and activates next player's turn
+  socket.on("playerTurn", function(player_data){
     cardDraw();
     var num;
-    if(data["playerNum"] == nbOfPlayers-1){
+
+    if (player_data.playerNum == nbOfPlayers-1)
       num = 0;
-    }
-    else{
-      num = data["playerNum"]+1;
-    }
-    players[data["playerNum"]].status = 0;
+    else
+      num = player_data.playerNum + 1;
+
+    players[player_data.playerNum].status = 0;
     players[num].status = 1;
-    io.emit('status', {"playerStatus": 1,
-                       "playerName": players[num].aliasName,
-                       "playerNum": num }
-                      );
-    io.emit('status', {"playerStatus": players[data["playerNum"]].status,
-                       "playerName": players[data["playerNum"]].aliasName,
-                       "playerNum": data["playerNum"] }
-                      );
+
+    io.emit("status",
+      {"playerStatus": 1,
+       "playerName": players[num].aliasName,
+       "playerNum": num
+     });
+
+    io.emit("status",
+      {"playerStatus": players[player_data.playerNum].status,
+       "playerName": players[player_data.playerNum].aliasName,
+       "playerNum": player_data.playerNum
+      });
   });
 
 });
